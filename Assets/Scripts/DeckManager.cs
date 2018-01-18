e4b4d85a03a177f83de561cb2d81d0fcea397215
Iza <izanagi@ucla.edu>
2018-01-17 19:57:17 -0800

    DeckManager
    Added to DeckManager

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 //Represents a deck of cards
 public class DeckManager : MonoBehaviour {
-    List<PlayerCard> deck;
+    
+	List<PlayerCard> deck;
+	List<PlayerCard> graveyard;
+
     // Use this for initialization
     void Start () {
-		
+		// TODO: create copies of each card 
+		deck.Add(new Card4_SelfDestruct());
+		deck.Add (new Card5 ());
 	}
 	
     void drawCard(HandManager hm)
     {
-        hm.insertCard(deck[0]);
-        deck.RemoveAt(0);
+		if (deck.Count > 0) {
+			hm.insertCard (deck [0]);
+			deck.RemoveAt (0);
+		} else {
+			// recreate deck from discarded cards
+			recreateDeck();
+		}
     }
+
+	// this function should by called whatever class uses & discards a card
+	public void discardCard(PlayerCard card) {  
+		graveyard.Add (card);
+	}
+
+	void recreateDeck() {
+		// shuffle graveyard, insertback into deck - randomly select/remove and Add to deck
+		while (graveyard.Count > 0) {
+			int index = Random.Range (0, graveyard.Count);
+			deck.Add (graveyard [index]);
+			graveyard.RemoveAt (index);
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
